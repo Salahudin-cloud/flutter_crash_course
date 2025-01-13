d393d51c470bc471446df24714c440fd94ccd5e8
Salahudin-cloud <ayubi54321@gmail.com>
2025-01-13 18:56:08 +0700

    learn colums

--- lib/home.dart
+++ lib/home.dart
@@ -7,11 +7,37 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Flutter crash"),
-        backgroundColor: Colors.amber,
+        title: const Text(
+          "My Coffee Id",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.brown[700],
         centerTitle: true,
       ),
-      body: const Text("Hello world!, from flutter"),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(20),
+            color: Colors.brown[200],
+            child: const Text(
+              "How i like my coffee",
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            color: Colors.brown[300],
+            child: const Text(
+              "Coffee prefs.",
+              style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,61 @@ import 'package:flutter_crash_course/home.dart';
 void main() {
   runApp(const MaterialApp(home: Home()));
 }
+
+class Sanbox extends StatelessWidget {
+  const Sanbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Sanbox"),
+        backgroundColor: Colors.grey,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(20),
+            color: Colors.red,
+            width: 100,
+            margin: const EdgeInsets.only(top: 5),
+            child: const Text(
+              "Box 1",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            color: Colors.blue,
+            width: 200,
+            margin: const EdgeInsets.only(top: 5),
+            child: const Text(
+              "Box 2",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            color: Colors.green,
+            width: 100,
+            margin: const EdgeInsets.only(top: 5),
+            child: const Text(
+              "Box 3",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
